ec4aca74c24ecf6d2c2b99287787116e50f3630b
Yesha Pandya <yesha.pandya@outlook.com>
2024-07-29 14:32:18 +0530

    Create 01_hello.java

---
+++ 01_hello.java
@@ -0,0 +1,11 @@
+//simple program to print hello world in java
+
+class hello{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
+
+//NOTE that the class containing main function should have same name as file name (hello in this case)
+//this is because since execution of program starts with main, it helps compiler locate where main function is (and that class should be declared in public only
+//if file name is different from the class containing main, error will occur
