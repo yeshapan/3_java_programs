54dc9cf26ae728f9493c420d50d8482efc20c551
Yesha Pandya <yesha.pandya@outlook.com>
2024-07-29 15:24:14 +0530

    Update and rename 02_notes.txt to 02_print.java

--- 02_print.java
+++ 02_print.java
@@ -1,25 +1,34 @@
-Java Text Output Methods
+/*Java Text Output Methods
 
   1. Text Output
 
         i) println: adds new line at end of output
-            example:
+            example:*/
                     System.out.println("Hello World!");
                     System.out.println("I am learning Java.");
                     System.out.println("It is awesome!");
 
-        ii) print: does not add new line at end of output
-            example:
+       /*ii) print: does not add new line at end of output
+            example:*/
                     System.out.print("Hello World! ");
                     System.out.print("I will print on the same line.");
 
-NOTE: try to prefer using println for clarity in output
+/*NOTE: try to prefer using println for clarity in output
 
   2. Numbers Output
         we can also use println to output numbers
-        examples:
+        examples:*/
                 System.out.println(3);
                 System.out.println(358);
                 System.out.println(3 + 3);
                 System.out.println(2 * 5);
+
+//JAVA COMMENTS
+//single line comment
+
+/* 
+multi-line comment
+*/
+
+
                 
