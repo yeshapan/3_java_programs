1f04964e2d797768e175039a4a5c353ff91f24ac
Yesha Pandya <yesha.pandya@outlook.com>
2024-07-29 16:04:41 +0530

    Update 03_variables.java

--- 03_variables.java
+++ 03_variables.java
@@ -27,3 +27,18 @@ use the final keyword (this will declare the variable as "final" or "constant",
 which means unchangeable and read-only):*/
 final int MyNum=10;
 MyNum=20; //will generate error: cannot assign value to final variable
+
+//we can use '+' to combine two variables
+String firstname="Yesha";
+String lastname="Pandya";
+String fullname= firstname+lastname;
+System.out.println(fullname);
+
+//displaying variables
+int num1=20;
+int num2=30;
+System.out.println(num1+num2); //will give sum output
+
+//we can use + to join string and a variable
+String name="Yesha";
+System.out.println("Hello "+name);
